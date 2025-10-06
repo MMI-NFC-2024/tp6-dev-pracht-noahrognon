1cd451acc7416dcda8eea86a630283655ca2bc61
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-10-06 17:08:11 +0200

    oublie de commit

---
+++ src/utils/slug.ts
@@ -0,0 +1,15 @@
+export const slugify = (value: string) =>
+  value
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)/g, "");
+
+export const deslugify = (value: string) =>
+  value
+    .split("-")
+    .filter(Boolean)
+    .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1))
+    .join(" ");
+
